6464c6a7c7fe8360dcf8c546cb5b16637b9c07e0
Manivarma Mudunuru <manivarma26@gmail.com>
2025-11-04 12:29:13 +0530

    Initial commit: AWS Infra skeleton

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Example: ECR repo for hardened images
+module "ecr" {
+  source = "./modules/ecr"
+}
